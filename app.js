07b4bd047bee46dc180364e486409d2ea87ed5e7
Akash <akashdelhi007@gmail.com>
2021-11-28 20:35:05 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 var cors = require("cors");
+const path = require('path');
+
 
 var corsOptions = {
   origin: "http://localhost:3000",
@@ -23,12 +25,18 @@ app.use(express.json());
 // require router/route file
 app.use('/app',require("./router/auth"));
 
+app.use(express.static("parking/build"))
+
+
+app.get('*', (_, res) => res.sendFile(path.resolve('parking', 'build', 'index.html')));
+
 //import port no. from env file
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV == "production"){
-  app.use(express.static("parking/build"))
-}
+
+// if(process.env.NODE_ENV == "production"){
+// }
+
 
 app.listen(PORT, () => {
   console.log(`server has started on port no. ${PORT}`);
